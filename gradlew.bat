b4658259425398dacc12aa80bad959686f4c053f
prodipsarkar5911-droid <prodipsarkar5911@gmail.com>
2026-08-30 13:58:57 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,11 @@
-
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem  Gradle startup script for Windows
+@rem ##########################################################################
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+java -version
+"%JAVA_HOME%\bin\java.exe" -version
+java -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
